7bf79b44114d9623eaf43fcdaa8e2a489e69e96f
João Carlos Fernandes Neto <wysrar@gmail.com>
2017-09-20 13:35:23 -0400

    Initial Commit

---
+++ cours 12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cours 12
+//
+//  Created by João Carlos Fernandes Neto on 17-09-20.
+//  Copyright © 2017 João Carlos Fernandes Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
