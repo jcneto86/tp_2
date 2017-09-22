8fa03134a6c305d73d85383765bde96b86170534
João Carlos Fernandes Neto <wysrar@gmail.com>
2017-09-22 15:02:35 -0400

    v1.0

--- cours 12/ViewController.swift
+++ cours 12/ViewController.swift
@@ -31,74 +31,79 @@ class ViewController: UIViewController {
     var arrayOfShowingBacks = [UIView]()
     var arrayOfHidingFronts = [UIView]()
     
-    //-----------------------
+    var cards: [UIView]!
+    
+    //-------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        cards = [card_1, card_2, card_3, card_4]
         arrayOfImageViews = [imgView1, imgView2, imgView3, imgView4]
         randomAnimals()
         setImagesToCards()
     }
-    //-----------------------
+    //-------------------
     @IBAction func showCard(_ sender: UIButton) {
         if arrayOfHidingFronts.count == 2 {
             return
         }
+        
         switch sender.tag {
-            case 0:
-                flipCard(from: front_1, to: back_1)
-                arrayOfCards.append(card_1)
-                arrayOfShowingBacks.append(back_1)
-                arrayOfHidingFronts.append(front_1)
-            case 1:
-                flipCard(from: front_2, to: back_2)
-                arrayOfCards.append(card_2)
-                arrayOfShowingBacks.append(back_2)
-                arrayOfHidingFronts.append(front_2)
-            case 2:
-                flipCard(from: front_3, to: back_3)
-                arrayOfCards.append(card_3)
-                arrayOfShowingBacks.append(back_3)
-                arrayOfHidingFronts.append(front_3)
-            case 3:
-                flipCard(from: front_4, to: back_4)
-                arrayOfCards.append(card_4)
-                arrayOfShowingBacks.append(back_4)
-                arrayOfHidingFronts.append(front_4)
-            default:
-                break
+        case 0:
+            flipCard(from: front_1, to: back_1)
+            arrayOfCards.append(card_1)
+            arrayOfShowingBacks.append(back_1)
+            arrayOfHidingFronts.append(front_1)
+        case 1:
+            flipCard(from: front_2, to: back_2)
+            arrayOfCards.append(card_2)
+            arrayOfShowingBacks.append(back_2)
+            arrayOfHidingFronts.append(front_2)
+        case 2:
+            flipCard(from: front_3, to: back_3)
+            arrayOfCards.append(card_3)
+            arrayOfShowingBacks.append(back_3)
+            arrayOfHidingFronts.append(front_3)
+        case 3:
+            flipCard(from: front_4, to: back_4)
+            arrayOfCards.append(card_4)
+            arrayOfShowingBacks.append(back_4)
+            arrayOfHidingFronts.append(front_4)
+        default:
+            break
         }
         arrayChosenCards.append(arrayOfRandomAnimalNames[sender.tag])
         verification()
     }
-    //-----------------------
+    //-------------------
     func flipCard(from: UIView, to: UIView) {
         let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
-            UIView.transition(with: from, duration: 1.0, options: transitionOptions, animations: {
-                from.isHidden = true
+        
+        UIView.transition(with: from, duration: 1.0, options: transitionOptions, animations: {
+            from.isHidden = true
         })
-            UIView.transition(with: to, duration: 1.0, options: transitionOptions, animations: {
-                to.isHidden = false
+        
+        UIView.transition(with: to, duration: 1.0, options: transitionOptions, animations: {
+            to.isHidden = false
         })
     }
-    //-----------------------
+    //-------------------
     func setImagesToCards() {
         var number = 0
         for imgView in arrayOfImageViews {
-            imgView.image = UIImage (named: arrayOfRandomAnimalNames[number])
+            imgView.image = UIImage(named: arrayOfRandomAnimalNames[number])
             number = number + 1
         }
     }
-    
-    //-----------------------
+    //-------------------
     func randomAnimals() {
         let numberOfAnimals = arrayOfAnimalNames.count
         for _ in 1...numberOfAnimals {
-            let randomNumer = Int(arc4random_uniform(UInt32(arrayOfAnimalNames.count)))
-            arrayOfRandomAnimalNames.append(arrayOfAnimalNames[randomNumer])
-            arrayOfAnimalNames.remove(at: randomNumer)
+            let randomNumber = Int(arc4random_uniform(UInt32(arrayOfAnimalNames.count)))
+            arrayOfRandomAnimalNames.append(arrayOfAnimalNames[randomNumber])
+            arrayOfAnimalNames.remove(at: randomNumber)
         }
-        }
-    //-----------------------
+    }
+    //-------------------
     func resetCards() {
         if arrayOfShowingBacks.count == 2 {
             Timer.scheduledTimer(timeInterval: 2,
@@ -108,7 +113,7 @@ class ViewController: UIViewController {
                                  repeats: false)
         }
     }
-    //-----------------------
+    //-------------------
     func verification() {
         if arrayChosenCards.count == 2 {
             if arrayChosenCards[0] == arrayChosenCards[1] {
@@ -124,13 +129,13 @@ class ViewController: UIViewController {
         }
         resetCards()
     }
-    //-----------------------
+    //-------------------
     @objc func hideCards() {
         arrayOfCards[0].isHidden = true
         arrayOfCards[1].isHidden = true
-        arrayChosenCards = []
+        arrayOfCards = []
     }
-    //-----------------------
+    //-------------------
     @objc func reFlip() {
         for index in 0..<arrayOfShowingBacks.count {
             flipCard(from: arrayOfShowingBacks[index], to: arrayOfHidingFronts[index])
@@ -138,9 +143,16 @@ class ViewController: UIViewController {
         arrayOfShowingBacks = []
         arrayOfHidingFronts = []
     }
-    //-----------------------
-
-
-    
+    //-------------------
+    @IBAction func reset(_ sender: UIButton) {
+        for card in cards {
+            card.isHidden =  false
+        }
+        arrayOfAnimalNames = ["panda.png", "panda.png", "penguin.png", "penguin.png"]
+        arrayOfRandomAnimalNames = []
+        randomAnimals()
+        setImagesToCards()
+    }
+    //-------------------
 }
 //==============================================
